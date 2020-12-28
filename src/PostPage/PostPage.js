caa4a46fdbf04c718fd5a5e81a87b3eca599f625
Lindsay Esterman <lindsayesterman@gmail.com>
2020-12-27 19:25:11 -0500

    project page

--- src/PostPage/PostPage.js
+++ src/PostPage/PostPage.js
@@ -2,12 +2,27 @@ import React, { Component } from "react";
 import Header from "../Header/Header.js";
 import "./PostPage.css";
 
-export default class Post extends Component {
+export default class Post extends Component { 
+  state = {
+    error:null,
+  }
+
+  handlePostProject = (e) => {
+    e.preventDefault();
+    const project = {
+      name: e.target["name"].value,
+      description: e.target["password"].value,
+      features: e.target["features"].value,
+      users: e.target["users"].value,
+      author: e.target["author"].value
+    }
+  }
+
   render() {
     return (
       <div className="post">
         <Header />
-        <form className="project-proposal" action="javascript:void(0);">
+        <form className="project-proposal" onSubmit={this.handlePostProject}>
           <h1 className="post-h1">Post a project</h1>
           <div className="form-input-material">
             <input
@@ -21,8 +36,8 @@ export default class Post extends Component {
           <div className="form-input-material">
             <textarea
               type="text"
-              name="descrip"
-              id="descrip"
+              name="description"
+              id="description"
               placeholder="App that allows users to post weekly screentimes and compete with friends for the lowest times of the week."
               required
             />
